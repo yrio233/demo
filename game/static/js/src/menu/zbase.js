d17c6a15f0b903fb8e4811d50166eb1086335986
yr s <1559820822@qq.com>
2022-10-20 09:32:32 +0800

    comelete menu background

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -45,10 +45,3 @@ class AcGameMenu{
     }
 
 }
-class AcGame {
-    constructor(id){
-        this.id = id;
-        this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
-    }
-}
